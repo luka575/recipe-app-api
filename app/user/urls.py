9ed9a3d86440755f277908a5cc6a55cce0781bb7
Luka Latinovic <luka.latinovic.21@singimail.rs>
2024-10-09 18:02:30 +0200

    Implement create user API Codechecker

---
+++ app/user/urls.py
@@ -0,0 +1,13 @@
+"""
+URL mappings for the user API.
+"""
+from django.urls import path
+
+from user import views
+
+
+app_name = 'user'
+
+usrpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+]
